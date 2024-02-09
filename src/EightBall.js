f16fa6317de36dc0ee101dcefbe90ef7a7b37397
emeyekae <mkaleski@yahoo.com>
2024-02-09 16:24:20 -0500

    adding react-eightball project assignment

---
+++ src/EightBall.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import './EightBall.css'
+
+
+
+EightBall.defaultProps = {
+    answers: [
+        { msg: "It is certain.", color: "green" },
+        { msg: "It is decidedly so.", color: "green" },
+        { msg: "Without a doubt.", color: "green" },
+        { msg: "Yes - definitely.", color: "green" },
+        { msg: "You may rely on it.", color: "green" },
+        { msg: "As I see it, yes.", color: "green" },
+        { msg: "Most likely.", color: "green" },
+        { msg: "Outlook good.", color: "green" },
+        { msg: "Yes.", color: "green" },
+        { msg: "Signs point to yes.", color: "goldenrod" },
+        { msg: "Reply hazy, try again.", color: "goldenrod" },
+        { msg: "Ask again later.", color: "goldenrod" },
+        { msg: "Better not tell you now.", color: "goldenrod" },
+        { msg: "Cannot predict now.", color: "goldenrod" },
+        { msg: "Concentrate and ask again.", color: "goldenrod" },
+        { msg: "Don't count on it.", color: "red" },
+        { msg: "My reply is no.", color: "red" },
+        { msg: "My sources say no.", color: "red" },
+        { msg: "Outlook not so good.", color: "red" },
+        { msg: "Very doubtful.", color: "red" },
+        { msg: "No way!", color: "red" },
+        { msg: "Ha Ha!! You're kidding... right?", color: "blue" },
+        { msg: "Only on Tuesdays.", color: "orange" },
+        { msg: "Of course! ", color: "green" },
+    ]
+}
+
+function EightBall({answers}) {
+    const [answer, setAnswer] = useState({
+       msg: "Think of a Question.",
+       color: "black", 
+    })
+
+function handleClick(evt) {
+    setAnswer(randomAns(answers));
+}
+
+function randomAns(arr) {
+  let randomIdx = Math.floor(Math.random() * arr.length);
+  return arr[randomIdx];
+}
+
+return ( 
+  <div
+      className="EightBall" 
+      onClick={handleClick} 
+      style = {{backgroundColor: answer.color}} 
+  >
+     <b>{answer.msg}</b>
+  </div>
+);
+}
+
+  export default EightBall;
